e43475755f7f2420f285e1376e828e2eb9961fde
Reda amine Saf <rsaf@e3r2p9.1337.ma>
2025-01-18 17:07:17 +0100

    focus on UBNOO

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -1,14 +1,18 @@
 # Update The mirroList
-echo "Update mirror"
+# echo "Update mirror"
 # sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 # sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 # sudo yum update -y
 
 
-# # curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --flannel-backend none --token 12345
-# sudo /usr/local/bin/k3s-uninstall.sh
-# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-backend none --node-ip 192.168.56.110 --write-kubeconfig-mode 0644" K3S_TOKEN=12345 sh -s -
+echo "Hello World"
+
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --token 12345
+sudo /usr/local/bin/k3s-uninstall.sh
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server  --node-ip 192.168.56.110 --write-kubeconfig-mode 0644" K3S_TOKEN=12345 sh -s -
 # sudo kubectl get all -n kube-system
+
+echo ".......................... Done"
 
 # # The connection to the server 127.0.0.1:6443 was refused - did you specify the right host or port?
 # # Job for k3s.service failed. See 'systemctl status k3s.service' and 'journalctl -xn' for details.
